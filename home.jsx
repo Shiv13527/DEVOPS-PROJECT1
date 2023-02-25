0414fb24a427d1c32d45465ad5d987923fb05260
Shiv13527 <shivpratappundir97@gmail.com>
2023-02-25 22:21:02 +0530

    Create home.jsx

---
+++ home.jsx
@@ -0,0 +1,10 @@
+import { Col, Container, Row, Spinner} from 'react-bootstrap';
+import HomeAside from '../components/HomeAside';
+import Timeline from '../components/Timeline';
+
+
+const Home = () => {
+
+};
+
+export default Home;
